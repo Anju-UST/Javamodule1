3d13b516000d7f41f5748eac46cdec76fdc33be1
Anju-UST <Anju.Reji@ust.com>
2023-03-11 12:26:41 +0530

    changes made new class is added in javafirstweekcodes

---
+++ javafirstweekcodes/src/javafirstweekcodes/example.java
@@ -0,0 +1,11 @@
+package javafirstweekcodes;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java");
+
+	}
+
+}
